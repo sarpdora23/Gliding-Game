fb86b6d75c8a068b5a64fdeb2d6430a4f299241f
sarpdora23 <69794292+sarpdora23@users.noreply.github.com>
2021-09-19 00:43:20 +0300

    Add files via upload
    Player uçarkenki rotate animasyonu olsun kanat açıp kapama animasyonları olsun uçarkenki sağ sola kontroll olsun hepsi bu scriptde yapılıyor. Öncelikle player havalandıktakn sonra yüksek bir devir ile dönmeye başlıyor (Rotate fonksiyonu) daha sonra oyuncu ekrana dokunduğu anda smooth bir şekilde player rotation'ı default pozisyona geçiyor bu geçişi do tween ile yapıyor. Uçarken player'ı sağ sola doğru kontroll ettirmeden önce bir delay giriyor bu delay oyunun daha smooth durması için. Daha sonra oyuncu parmağını sağa sola çekerek hareket edebiliyor. Bu hareket sırasında sağ sola animasyon çalışıyor bu animasyon sistemini daha dinamik olması için Lerp fonksiyonu ile yaptım. Karakterin sağ ve sol hareketi için "rb.velocity" değiştirerek yaptım.

---
+++ BallFly.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BallFly : MonoBehaviour
+{
+    [SerializeField]
+    private Animator ball_anim;
+    [SerializeField]
+    private Animator cam_anim;
+    private bool one_time_setCameraPosition;
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private float rotation_speed;
+    private bool canRotate;
+    private bool canControll;
+    [SerializeField]
+    private float horizontal_Speed;
+    private Rigidbody rb_body;
+    private float lerp_t;
+    private void Awake()
+    {
+        rb_body = gameObject.GetComponent<Rigidbody>();
+    }
+    private void Start()
+    {
+        one_time_setCameraPosition = true;
+        DOTween.Init();
+        canRotate = true;
+        canControll = false;
+        lerp_t = 0.5f;
+    }
+    private void Update()
+    {
+        if (GameManager.gameManager_Instance.GetCurrentState() == GameManager.GameStates.BALL_FLY)
+        {
+            ControllBall();
+            SetCameraPosition();
+            RotateBall();
+            OpenWings();
+            ControllBall();
+        }
+    }
+    void SetCameraPosition()
+    {
+        if (one_time_setCameraPosition)
+        {
+            cam_anim.Play("SetCameraPosition");
+        }
+    }
+    void RotateBall()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            lerp_t = 0.5f;
+            canControll = false;
+            ball_anim.SetTrigger("Close");
+            canRotate = true;
+        }
+        if (canRotate)
+        {
+            ball.transform.Rotate(new Vector3(rotation_speed * Time.deltaTime * 10, 0, 0));
+        }
+        
+    }
+    void OpenWings()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            canRotate = false;
+            ball_anim.ResetTrigger("Close");//56.309
+            ball.transform.DORotateQuaternion(Quaternion.Euler(45.7165f, -4.741f, -6.596f),0.7f).OnComplete(()=> ball_anim.Play("OpenWing"));
+            StartCoroutine(OpenControllDelay());
+        }
+    }
+    IEnumerator OpenControllDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        canControll = true;
+    }
+    void ControllBall()
+    {
+        if (canControll)
+        {
+            //LEFT --> X: 49.167, Y: 31.964, 39,513
+            //RIGHT --> X:42.266, Y: -41.446, -52.705
+            float h = Input.GetAxisRaw("Mouse X");
+            Vector3 temp_rot = ball.transform.eulerAngles;
+            lerp_t += h * Time.deltaTime;
+            lerp_t = Mathf.Clamp(lerp_t, 0, 1);
+            temp_rot.x = Mathf.Lerp(49.167f, 42.266f, lerp_t);
+            temp_rot.y = Mathf.Lerp(31.964f, -41.446f, lerp_t);
+            temp_rot.z = Mathf.Lerp(39.513f, -52.705f, lerp_t);
+            ball.transform.eulerAngles = temp_rot;
+            if (lerp_t > 0.5f)
+            {
+                rb_body.velocity = new Vector3(horizontal_Speed, rb_body.velocity.y, rb_body.velocity.z);
+            }
+            else if (lerp_t < 0.5f)
+            {
+                rb_body.velocity = new Vector3(-horizontal_Speed, rb_body.velocity.y, rb_body.velocity.z);
+            }
+        }
+    }
+}

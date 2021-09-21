26e2e082a56fc92c0d5c1518832151f6bab6b2cc
sarpdora23 <69794292+sarpdora23@users.noreply.github.com>
2021-09-21 22:43:10 +0300

    Update LevelGenerator.cs
    Random level generator için aklıma şöyle bir fikir geldi önceden bir sıra bir sıra bir sıra diye oluşturuyordum fakat böyle yapınca player x ekseni üzerinde hareket edince sağa ve sola nasıl generate edeceğimi bulamadım bende şöyle düşündüm bir sıra yerine 3 sıra yapıyım daha sonra ortada kalan sıranın uçlarının konumunu tutayım eğer o sınırları player geçerse yandaki sıraları diğer tarafa ışınlamayı düşündüm hala bitmedi ama yarın biter diye düşünüyorum aksilik olmazsa sistem 3 aşağı 5 yukarı böyle olcak

--- LevelGenerator.cs
+++ LevelGenerator.cs
@@ -16,8 +16,11 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField]
     private Transform first_spawn_transform;
     private Vector3 spawn_position;
-    private GameObject[] lines_array = new GameObject[5];
-    private int counter;
+    private GameObject[][] lines_array = new GameObject[5][];
+    private int z_counter, min_x_index,max_x_index;
+    [SerializeField]
+    private Transform level_parent;
+    private Vector3 min_Censor, max_Censor;
     public static LevelGenerator levelGenerator_Instance
     {
         get
@@ -35,45 +38,106 @@ public class LevelGenerator : MonoBehaviour
     }
     private void Awake()
     {
+        for (int i = 0; i < 5; i++)
+        {
+            lines_array[i] = new GameObject[3];
+        }       
         spawn_position = first_spawn_transform.position;
-        counter = 0;
+        z_counter = 0;
+        min_x_index = 0;
+        max_x_index = 2;
     }
     private void Start()
     {
         for (int i = 0; i < 5; i++)
         {
-           GameObject line_instance= Instantiate(line, spawn_position, Quaternion.identity);
-           lines_array[i] = line_instance;
-           spawn_position.z += 111; 
+            Vector3 temp_position = spawn_position;
+            temp_position.x -= 60;
+            for (int j = 0; j < 3; j++)
+            {
+                GameObject obj = Instantiate(line, temp_position, Quaternion.identity);
+                obj.transform.parent = level_parent;
+                lines_array[i][j] = obj;
+                temp_position.x += 360;
+            }
+            spawn_position.z += 111;
         }
        
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (player_ball.position.x > max_Censor.x)
         {
-            GenerateNewLine();
+            GenerateNewLineX(true);
+        }
+        else if (player_ball.position.x < min_Censor.x)
+        {
+            GenerateNewLineX(false);
         }
     }
     public void IsVisible(Transform obj)
     {
         if (obj.position.z < player_ball.position.z)
         {
-            GenerateNewLine();
-        }        
+            GenerateNewLineZ();
+        }
     }
-    private void GenerateNewLine()
+    private void GenerateNewLineZ()
     {
-        Debug.Log("Hayda");
-        lines_array[counter].transform.position = spawn_position;
-        spawn_position.z += 111;
-        if (counter == 4)
+        foreach (GameObject line in lines_array[z_counter])
         {
-            counter = 0;
+            Vector3 temp_position = line.transform.position;
+            temp_position.z = spawn_position.z;
+            line.transform.position = temp_position;
+        }
+        spawn_position.z += 111;
+        if (z_counter == 4)
+        {
+            z_counter = 0;
         }
         else
         {
-            counter++;
+            z_counter++;
         }
+
+    }
+    private void GenerateNewLineX(bool isBigger)
+    {
+        if (isBigger)
+        {
+            for (int i = 0; i < lines_array.Length; i++)
+            {
+                Vector3 temp_position = lines_array[i][max_x_index].transform.position;
+                temp_position.x += 360;
+                lines_array[i][min_x_index].transform.position = temp_position;
+            }
+            max_x_index = min_x_index;
+            if (min_x_index == 2)
+            {
+                min_x_index = 0;
+            }
+            else
+            {
+                min_x_index++;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < lines_array.Length; i++)
+            {
+                Vector3 temp_position = lines_array[i][min_x_index].transform.position;
+                temp_position.x -= 300;
+                lines_array[i][max_x_index].transform.position = temp_position;
+            }
+            min_x_index = max_x_index;
+            if (max_x_index == 0)
+            {
+                max_x_index = 2;
+            }
+            else
+            {
+                max_x_index--;
+            }
+        }       
     }
 }
